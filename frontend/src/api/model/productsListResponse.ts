98ba1c6c2c2e85674887532afa7c91e6583f81bf
bvquoc <bvquoc.dev@gmail.com>
2024-06-15 03:06:42 +0700

    rm FE

--- frontend/src/api/model/productsListResponse.ts
+++
@@ -1,7 +0,0 @@
-import type { Product } from './product'
-import type { Metadata } from './metadata'
-
-export interface ProductsListResponse {
-  data?: Product[]
-  metadata?: Metadata
-}
--- frontend/src/constants/routes.ts
+++
@@ -1,7 +0,0 @@
-export const ROUTES = {
-  LOGIN: '/login',
-  DASHBOARD: '/',
-  FORMS: '/forms',
-  DATA_FETCHING: '/data-fetching',
-  PRODUCTS: '/products',
-}
